14b6b7e664f02bb8db144f896f77988ba5333e5d
SachinVAST <csachin@valueaddsofttech.com>
2023-05-16 18:43:47 +0530

    Added categories Service

--- src/services/index.js
+++ src/services/index.js
@@ -1,6 +1,10 @@
+import { categories } from './categories/categories.js'
+
 import { user } from './users/users.js'
 
 export const services = (app) => {
+  app.configure(categories)
+
   app.configure(user)
 
   // All services will be registered here
